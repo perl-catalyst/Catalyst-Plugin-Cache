204b2b54f823e7a4ca8c0038a28432d78693bef8
Yuval Kogman <nothingmuch@woobling.org>
2008-01-27 03:47:36 +0000

    changelog and version bump

--- lib/Catalyst/Plugin/Cache.pm
+++ lib/Catalyst/Plugin/Cache.pm
@@ -6,7 +6,7 @@ use base qw(Class::Accessor::Fast Class::Data::Inheritable);
 use strict;
 use warnings;
 
-our $VERSION = "0.04";
+our $VERSION = "0.05";
 
 use Scalar::Util ();
 use Catalyst::Utils ();

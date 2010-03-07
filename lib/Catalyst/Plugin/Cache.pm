1c99d822a8a945b775b496d492f87cface32e24c
Tomas Doran <bobtfish@bobtfish.net>
2010-03-07 20:44:47 +0000

    Checking in changes prior to tagging of version 0.09.  Changelog diff is:
    Index: Changes
    ===================================================================
    --- Changes	(revision 13014)
    +++ Changes	(working copy)
    @@ -1,3 +1,4 @@
    +0.09
    - Generate a warning if no config is specified, or config
    is specified using the old key.
    - Support the compute() method, and emulate it if the backend

--- lib/Catalyst/Plugin/Cache.pm
+++ lib/Catalyst/Plugin/Cache.pm
@@ -6,7 +6,7 @@ use base qw(Class::Accessor::Fast Class::Data::Inheritable);
 use strict;
 use warnings;
 
-our $VERSION = "0.08";
+our $VERSION = "0.09";
 
 use Scalar::Util ();
 use Catalyst::Utils ();

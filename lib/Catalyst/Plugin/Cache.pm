a78b4047d4527467735bfe53cc516a3f283b9f9b
Tomas Doran <bobtfish@bobtfish.net>
2010-05-20 16:47:27 +0000

    Fix bug, I think

--- lib/Catalyst/Plugin/Cache.pm
+++ lib/Catalyst/Plugin/Cache.pm
@@ -12,7 +12,7 @@ use Scalar::Util ();
 use Catalyst::Utils ();
 use Carp ();
 use MRO::Compat;
-
+use Scalar::Util qw/ blessed /;
 use Catalyst::Plugin::Cache::Curried;
 
 __PACKAGE__->mk_classdata( "_cache_backends" );
@@ -154,7 +154,7 @@ sub cache {
     if ( @meta == 1 ) {
         my $name = $meta[0];
         return ( $c->get_preset_curried($name) || $c->get_cache_backend($name) );
-    } elsif ( !@meta ) {
+    } elsif ( !@meta && blessed $c ) {
         # be nice and always return the same one for the simplest case
         return ( $c->_default_curried_cache || $c->_default_curried_cache( $c->curry_cache( @meta ) ) );
     } else {
